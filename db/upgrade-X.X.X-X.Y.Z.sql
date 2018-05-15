edd6958818ab2f7fe544fbeeb5b2853bb05cccbd
Durand Fabrice <oeufdure@gmail.com>
2018-05-15 16:15:25 -0400

    Added provisioner ipsk

--- db/upgrade-X.X.X-X.Y.Z.sql
+++ db/upgrade-X.X.X-X.Y.Z.sql
@@ -49,3 +49,7 @@ call ValidateVersion;
 
 
 INSERT INTO pf_version (id, version) VALUES (@VERSION_INT, CONCAT_WS('.', @MAJOR_VERSION, @MINOR_VERSION, @SUBMINOR_VERSION)); 
+
+ALTER TABLE person
+    ADD `ipsk` varchar(255) NULL DEFAULT NULL
+;
