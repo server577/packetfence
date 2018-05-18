223b5e99b9e0eb3de7b75ac28c002b134df47be1
Durand Fabrice <oeufdure@gmail.com>
2018-05-17 20:21:59 -0400

    Fix for PR

--- db/upgrade-X.X.X-X.Y.Z.sql
+++ db/upgrade-X.X.X-X.Y.Z.sql
@@ -51,5 +51,5 @@ call ValidateVersion;
 INSERT INTO pf_version (id, version) VALUES (@VERSION_INT, CONCAT_WS('.', @MAJOR_VERSION, @MINOR_VERSION, @SUBMINOR_VERSION)); 
 
 ALTER TABLE person
-    ADD `ipsk` varchar(255) NULL DEFAULT NULL
+    ADD `psk` varchar(255) NULL DEFAULT NULL
 ;
